d6c4cedea10969f78aa4a0e96b65c852ceb73f9e
An <nguyenvanan7580@gmail.com>
2025-10-07 15:26:43 +0700

    fix path 2

--- back-end/src/routes/auth.js
+++ back-end/src/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import authController from '../controllers/authController.js';
+import authController from '../controllers/AuthController.js';
 
 const router = Router();
 //đăng nhập
