b45db16c91a649db83052861b76674a8d44855e7
An <nguyenvanan7580@gmail.com>
2025-10-07 22:32:05 +0700

    fix configs/db.js cho clever cloud

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -1,7 +1,5 @@
-// src/config/db.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const sequelize = new Sequelize(
@@ -10,7 +8,11 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }, // ✅ Bắt buộc cho Clever Cloud
+    },
     logging: false,
   }
 );
